2c5660111bde5d89ea4c9a3a5c40f558314634c2
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-08-25 11:41:12 +0300

    Added Recipe App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+import {createBrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "/recipe/:recipeId",
+    element: <RecipeDetails />
+  },
+]);
 
 function App() {
 
