55fb42970d86e8d532d6dc3b979a4368823d4401
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-08-31 15:14:13 +0300

    Added Fetching App

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,21 +2,24 @@ import { useQuery } from 'react-query';
 
 const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!res.ok) {
+        throw new Error('Network response was not ok');
+    }
     return res.json();
 };
 
 const PostsComponent = () => {
-    const { data, isError, isLoading, refetch } = useQuery('fetchPosts', fetchPosts);
+    const { data, error, isError, isLoading, refetch } = useQuery('fetchPosts', fetchPosts);
 
     if (isLoading) return <div>Loading...</div>;
 
-    if (isError) return <div>Error loading data</div>;
+    if (isError) return <div>Error loading data: {error.message}</div>;
 
     return (
         <div>
             <button onClick={() => refetch()}>Refetch Data</button>
             {data.map(item => (
-                <div key={item.id}>{item.title}</div>  
+                <div key={item.id}>{item.title}</div>
             ))}
         </div>
     );
