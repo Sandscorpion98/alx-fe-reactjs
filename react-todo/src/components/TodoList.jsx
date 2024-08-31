e70321b47af271ef99cd6ead7207f68de7f694ea
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-08-31 17:27:15 +0300

    Added Test TodoList App

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -23,14 +23,14 @@ const TodoList = () => {
     }
   };
 
- 
+  
   const toggleTodo = (id) => {
     setTodos(todos.map(todo =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
     ));
   };
 
-  
+ 
   const deleteTodo = (id) => {
     setTodos(todos.filter(todo => todo.id !== id));
   };
