2daa6c7882c47b7bacb182cbcb488b431c45f654
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-08-31 15:08:03 +0300

    Added Fetching App

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,25 @@
+import { useQuery } from 'react-query';
+
+const fetchData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return res.json();
+};
+
+const PostsComponent = () => {
+    const { data, error, isLoading, refetch } = useQuery('fetchData', fetchData);
+
+    if (isLoading) return <div>Loading...</div>;
+
+    if (error) return <div>Error loading data</div>;
+
+    return (
+        <div>
+            <button onClick={() => refetch()}>Refetch Data</button>
+            {data.map(item => (
+                <div key={item.id}>{item.title}</div>  
+            ))}
+        </div>
+    );
+};
+
+export default PostsComponent;
