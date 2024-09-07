2f64a3f17c50d78d0150e1d40997bedc42af13e3
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-09-07 19:45:46 +0300

    Added Tailwind To React App

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-sm md:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+      <div className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24" />
         <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
