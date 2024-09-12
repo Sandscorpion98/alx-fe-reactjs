c3e3b333aacd28b1a51e5783d184f4666f7e90da
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-09-12 21:37:10 +0300

    Added Tailwind to React App

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
