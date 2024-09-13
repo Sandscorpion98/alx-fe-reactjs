d2f954e96b892acd0496f16e56a2ad39ed75bdd0
Ahmed Hussien98 <sandscorpion98@gmail.com>
2024-09-13 17:58:54 +0300

    Added Tailwind to React App

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from "./components/RecipeDetail";
 import "./index.css"
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/form" element={<AddRecipeForm />} />
       </Routes>
     </Router>
   );
